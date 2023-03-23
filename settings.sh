30b71b26f7cf2cc11c1c8dfbb1abb1c94e2c058c
ivanmalyshev <ivanmalyshev1994@gmail.com>
2023-03-23 09:32:40 +0300

    done

---
+++ settings.sh
@@ -0,0 +1,21 @@
+# repo override
+#ru
+sed -i 's/us.archive.ubuntu.com/mirror.linux-ia64.org/g' /etc/apt/sources.list
+
+useradd $1 -s /bin/bash -d /home/mid
+mkdir /home/mid
+chown -R mid:mid /home/mid
+echo ''$1'    ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+usermod --password $(openssl passwd -6 $2) root
+usermod --password $(openssl passwd -6 $2) $1
+
+if [ $3 == "true" ]; then apt upgrade -y; else echo '$3'=$3; fi
+
+rm -Rf /etc/hosts
+
+echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+echo "$5	$4.localdomain	$4" >> /etc/hosts
+
+
+
